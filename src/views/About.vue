6bcfbd0feda5577d40ad87ab58b2e1a5b8597540
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-08 13:29:02 +0100

    Make the about page nicer

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>A todo list app that uses Vue and JSON placeholder.</p>
   </div>
 </template>
+
+<style scoped>
+.about {
+  padding: 10px;
+}
+
+h1 {
+  padding-bottom: 1rem;
+}
+</style>
